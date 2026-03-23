a1e25d2ceacb2c61a3baf2355b86be6ae736a240
Your Namealanreigns24 <alanfinep@gmail.com>
2026-03-23 10:07:57 +0530

    Fix calc again

--- calculator.py
+++ calculator.py
@@ -1,11 +1,4 @@
-"""Simple calculator module."""
-
-
 def add(a, b):
-    """Add two numbers."""
-    return a + b
-
-
+ return a + b
 def subtract(a, b):
-    """Subtract b from a."""
-    return a - b
+ return a - b
